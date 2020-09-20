fcfa7dc03ab32f6b67aa86af897bc62df1c13fba
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2020-09-20 10:13:03 +0900

    use clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap_v3::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("My RPN program")
+        .arg(
+            Arg::with_name("formula_file")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .short('v')
+                .long("verbose")
+                .required(false)
+        ).get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("file name is {}", file),
+        None => println!("no file")
+    };
+
+    let verbose = matches.is_present("verbose");
+    println!("verbose is {}", verbose);
 }
