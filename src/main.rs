f2b3d032edeff1887cdd0f0c21a43aab16d97b66
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2020-09-21 00:26:18 +0900

    add rpn struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use clap::{App, Arg};
 use clap::Clap;
+use std::fs::File;
+use std::io::{BufReader, BufRead, stdin};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -16,13 +17,73 @@ struct Opts {
     formula_file: Option<String>,
 }
 
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        };
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
+
 fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("file is {}", file),
-        None => println!("file not found"),
-    };
 
-    let verbose = opts.verbose;
-    println!("verbose is {}", verbose);
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        // ファイル指定なし
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator(verbose);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer = calc.eval(&line);
+        println!("{}", answer);
+    }
 }
