2484bb34b7372e0e2235ab350b36f5c799172fe9
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2020-09-21 16:49:52 +0900

    use anyhow crate

---
+++ src/bin/err_anyhow.rs
@@ -0,0 +1,18 @@
+use anyhow::{Context, Result};
+
+fn get_int_from_file() -> Result<i32> {
+    let path = "number.txt";
+
+    let num_str = std::fs::read_to_string(path).
+        with_context(|| format!("faild to read string from {}", path))?;
+
+    num_str.trim().parse::<i32>().map(|t| t * 2).context("faild to parse string")
+}
+
+fn main()
+{
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e)
+    }
+}
